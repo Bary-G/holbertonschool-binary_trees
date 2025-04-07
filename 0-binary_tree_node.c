b6ad273261b2adccb75a8d575120f68af8878907
Bary-G <b9059289@gmail.com>
2025-04-07 15:44:23 +0200

    task0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * _height - Measures the height of a binary tree
+ *
+ * @parent: binary tree
+ * @value: new node value
+ *
+ * Return: The height of the tree starting at @node
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->n = value;
+	newNode->parent = parent;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return (newNode);
+}
