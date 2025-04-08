517bcf39e011faa902992991280a7982d05819fa
Bary-G <b9059289@gmail.com>
2025-04-08 12:39:46 +0200

    header

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Delete the tree
+ *
+ * @tree: binary tree
+ *
+ * Return: Nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		tree->left == NULL;
+		tree->right == NULL;
+		tree->parent == NULL;
+	}
+
+}
