a7abb5e5b125e33e8010493918a9a0714e148cc0
loubassouM <loubassoumurphy@gmail.com>
2025-04-08 08:24:12 +0000

    1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,17 +3,15 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - insert child at left
- *
- * @parent: binary tree
- * @value: new child
- *
- * Return: The height of the tree starting at @node
- */
+* binary_tree_insert_left - insert child at left
+*
+*@parent: binary tree
+* @value: new child
+*
+* Return: The height of the tree starting at @node
+*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-
-	parent->left = binary_tree_node(parent, value);
-
-	return (parent->left);
+parent->left = binary_tree_node(parent, value);
+return (parent->left);
 }
